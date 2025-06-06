36841e7b30e787e7ccb21eef90ad3fb9a06e9e5a
vicente augusto <62485355+vicente321@users.noreply.github.com>
2025-06-06 12:21:59 -0300

    Update data.js

--- data.js
+++ data.js
@@ -5,6 +5,6 @@ const listaProdutos = [
   ["https://vicente321.github.io/imgs/monster_watermelon.webp", "Monster Ultra Wattermellow", "Sabor intenso de melancia que proporciona uma sensação refrescante;", "ENERGÉTICO", "7.52"],
   ["https://vicente321.github.io/imgs/monster_pipeline_punch.webp", "Monster Pipeline Punch", "O energético de sabor exótico, único, numa perfeita combinação dos sabores da goiaba, laranja e maracujá.", "ENERGÉTICO", "7.52"],
   ["https://vicente321.github.io/imgs/monster_pacific_punch.webp", "Monster Pacific Punch", "Sabor tropical que é leve, suave e o toque perfeito de doce", "ENERGÉTICO", "7.52"],
-  ["https://vicente321.github.io/imgs/CCSA_LT_220ML.webp", "Coca Zero", "", "BEBIDA", "2.40"],
-  ["https://vicente321.github.io/imgs/110255_COCA---CCO220ml.webp", "Coca Lata", "", "BEBIDA", "2.40"],
+  ["https://vicente321.github.io/imgs/CCSA_LT_220ML.webp", "Coca Zero", "", "REFRIGERANTE", "2.40"],
+  ["https://vicente321.github.io/imgs/110255_COCA---CCO220ml.webp", "Coca Lata", "", "REFRIGERANTE", "2.40"],
 ];
